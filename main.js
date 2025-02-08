aee7c90343f299a2b1c5860a91704b9b422efa91
LuisQuelhasDev <lf71@hotmail.com>
2025-02-08 10:06:27 -0300

    Commit inicial

---
+++ main.js
@@ -0,0 +1,22 @@
+const form = document.getElementById('form_input')
+let linhas = ''
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+
+    const inputNome = document.getElementById('nome')
+    const inputNumero = document.getElementById('numero')
+
+    let linha = '<tr>'
+    linha += `<td>${inputNome.value}</td>`
+    linha+= `<td>${inputNumero.value}</td>`
+    linha +='</tr>'
+
+    linhas += linha
+
+    const corpoTabela = document.querySelector('tbody')
+    corpoTabela.innerHTML = linhas
+
+    inputNome.value = ''
+    inputNumero.value = ''
+})
